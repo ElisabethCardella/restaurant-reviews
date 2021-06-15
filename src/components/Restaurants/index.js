a1bbf58eb72e4f2e379e406aaca5ea71409b58b2
ElisabethCardella <elisabeth.cardellav@gmail.com>
2021-06-15 09:41:17 +0200

    changes

--- src/components/Restaurants/index.js
+++ src/components/Restaurants/index.js
@@ -1,9 +1,33 @@
 import { getRestaurants } from "../../services/restaurants";
-import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td, Alert } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 const Restaurants = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const restaurants = getRestaurants();
-  console.log(restaurants);
+
+  function getAverage(ratings) {
+    const sum = ratings.reduce((sum, rating) => sum + rating.stars, 0);
+
+    return sum / ratings.length;
+  }
+
+  /*function getAverage(ratings) {
+    let sum = 0;
+    for (let i = 0; i < ratings.length; i++) {
+      sum = sum + ratings[i].stars;
+    }
+    return sum / ratings.length;
+  }*/
 
   return (
     <Table variant="simple">
@@ -18,9 +42,51 @@ const Restaurants = () => {
         {restaurants.map((restaurant) => {
           return (
             <Tr>
-              <Td>{restaurant.restaurantName}</Td>
+              <Td>
+                <Modal isOpen={isOpen} onClose={onClose}>
+                  <ModalOverlay />
+                  <ModalContent>
+                    <ModalHeader>{restaurant.restaurantName}</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                      <Table variant="simple">
+                        <Thead>
+                          <Tr>
+                            <Th>Notation </Th>
+                            <Th>Commentaires</Th>
+                          </Tr>
+                        </Thead>
+                        <Tbody>
+                          <Td>
+                            {restaurant.ratings.map((rating) => (
+                              <li key={rating.stars}>{rating.stars}</li>
+                            ))}
+                          </Td>
+                          <Td>
+                            {restaurant.ratings.map((rating) => (
+                              <li key={rating.comment}>{rating.comment}</li>
+                            ))}
+                          </Td>
+                        </Tbody>
+                      </Table>
+                    </ModalBody>
+                  </ModalContent>
+                </Modal>
+                <button
+                  onClick={() => {
+                    onOpen();
+                  }}
+                  /*if (restaurant.ratings.length > 4.5) {
+                      restaurant.ratings.forEach((element) => {
+                        console.log(element.stars);
+                      });
+                    }*/
+                >
+                  {restaurant.restaurantName}
+                </button>
+              </Td>
               <Td>{restaurant.address}</Td>
-              <Td>"3"</Td>
+              <Td>{getAverage(restaurant.ratings)}</Td>
             </Tr>
           );
         })}
@@ -28,5 +94,4 @@ const Restaurants = () => {
     </Table>
   );
 };
-
 export default Restaurants;
